e196c7f8d1b40653fb9f5654d85a66be58ec5636
Miraak7000 <stuff.lang.dd@gmail.com>
2019-05-17 16:56:59 +0200

    .

--- RDA/Data/RewardPool.cs
+++ RDA/Data/RewardPool.cs
@@ -21,21 +21,50 @@ namespace RDA.Data {
       this.ID = id;
       this.Weight = weight;
       this.Items = new List<String>();
-      var rewardItem = Program.Original.XPathSelectElement($"//Asset[Values/Standard/GUID={id}]");
-      switch (rewardItem.Element("Template").Value) {
-        case "QuestItem":
-          // ignore
-          break;
-        case "RewardPool":
-          break;
-        case "Product":
-          this.Items.Add(id);
-          break;
-        default:
-          throw new NotImplementedException(rewardItem.Element("Template").Value);
-      }
+      this.DiscoverItems(id);
+      //var rewardItem = Program.Original.XPathSelectElement($"//Asset[Values/Standard/GUID={id}]");
+      //switch (rewardItem.Element("Template").Value) {
+      //  case "QuestItem":
+      //    // ignore
+      //    break;
+      //  case "RewardPool":
+      //    break;
+      //  case "Product":
+      //    this.Items.Add(id);
+      //    break;
+      //  default:
+      //    throw new NotImplementedException(rewardItem.Element("Template").Value);
+      //}
     }
     #endregion
+
+    private void DiscoverItems(String value) {
+      var asset = Program.Original.XPathSelectElement($"//Asset[Values/Standard/GUID={value}]");
+      switch (asset.Element("Template").Value) {
+        case "RewardPool":
+          foreach (var item in asset.XPathSelectElements("Values/RewardPool/ItemsPool/Item/ItemLink")) {
+            this.DiscoverItems(item.Value);
+          }
+          break;
+        case "QuestItem":
+        case "ActiveItem":
+        case "ItemSpecialAction":
+        case "ItemSpecialActionVisualEffect":
+          // TODO: need to be implemented first
+          break;
+        case "CultureItem":
+        case "GuildhouseItem":
+        case "HarborOfficeItem":
+        case "TownhallItem":
+        case "ShipSpecialist":
+        case "Product":
+        case "VehicleItem":
+          if (!this.Items.Contains(value)) this.Items.Add(value);
+          break;
+        default:
+          throw new NotImplementedException(asset.Element("Template").Value);
+      }
+    }
 
     #region Public Methods
     public override String ToString() {
@@ -48,7 +77,7 @@ namespace RDA.Data {
       var result = new XElement("Reward");
       result.Add(new XAttribute("ID", this.ID));
       result.Add(new XAttribute("Weight", this.Weight));
-      result.Add(new XAttribute("Items", this.Items.Select(s => s)));
+      result.Add(new XElement("Items", this.Items.Select(s => new XElement("Item", s))));
       return result;
     }
     #endregion
